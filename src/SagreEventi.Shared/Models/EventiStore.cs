a5e83129514b454576bbb29f760a9c9478e39b09
Angelo Pirola <angelo9871@gmail.com>
2023-12-21 00:00:24 +0100

    Aggiunto progetto SHARED

---
+++ src/SagreEventi.Shared/Models/EventiStore.cs
@@ -0,0 +1,14 @@
+﻿namespace SagreEventi.Shared.Models;
+
+public class EventiStore
+{
+    public EventiStore()
+    {
+        DataOraUltimoSyncServer = DateTime.MinValue;
+
+        ListaEventi = new List<EventoModel>();
+    }
+
+    public List<EventoModel> ListaEventi { get; set; }
+    public DateTime DataOraUltimoSyncServer { get; set; }
+}
---
+++ src/SagreEventi.Shared/Models/EventoModel.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SagreEventi.Shared.Models;
+
+public class EventoModel
+{
+    //Campo: Id
+    public string Id { get; set; }
+
+    //Campo: Nome Evento
+    [Required(ErrorMessage = "Il nome dell'evento è obbligatorio")]
+    [Display(Name = "Nome Evento")]
+    [DataType(DataType.Text)]
+    public string NomeEvento { get; set; }
+
+    //Campo: Luogo Evento
+    [Required(ErrorMessage = "Il luogo dell'evento è obbligatorio")]
+    [Display(Name = "Luogo Evento")]
+    [DataType(DataType.Text)]
+    public string CittaEvento { get; set; }
+
+    //Campo: Data Inizio Evento
+    [Required(ErrorMessage = "La data di inizio dell'evento è obbligatoria")]
+    [Display(Name = "Data Inizio Evento")]
+    [DataType(DataType.Date)]
+    public Nullable<DateTime> DataInizioEvento { get; set; }
+
+    //Campo: Data Fine Evento
+    [Required(ErrorMessage = "La data di fine dell'evento è obbligatoria")]
+    [Display(Name = "Data Fine Evento")]
+    [DataType(DataType.Date)]
+    public Nullable<DateTime> DataFineEvento { get; set; }
+
+    //Campo: Descrizione Evento
+    [Required(ErrorMessage = "La descrizione dell'evento è obbligatoria")]
+    [Display(Name = "Descrizione Evento")]
+    [DataType(DataType.Text)]
+    public string DescrizioneEvento { get; set; }
+
+    //Campo: Evento concluso - Indica se l'evento è terminato
+    public bool EventoConcluso { get; set; }
+
+    //Campo: Data e ora di ultima modifica (utilizzato dal servizio applicativo)
+    public DateTime DataOraUltimaModifica { get; set; }
+}
